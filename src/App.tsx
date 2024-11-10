918afb0499f1855ce661056c9310f17db7bf2600
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-10 22:03:00 +0900

    useQuery study

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+// https://jsonplaceholder.typicode.com/todos
+import { useQuery } from "@tanstack/react-query"
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todo'],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+  });
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <h1>Start</h1>
+      {data.map((todo: Todo) => 
+        <div key={todo.id}>
+          <h3>{todo.title}</h3>
+          <p>{todo.completed ? "Completed" : "Not completed"}</p>
+        </div>
+      )}
     </div>
   )
 }
